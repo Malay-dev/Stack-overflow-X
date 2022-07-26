5115f6135b0a89dbe4f21cd451bc4eccb108b0c4
Malay <malaykumar2003@gmail.com>
2022-07-26 09:22:13 +0530

    clean

--- Client/src/components/Pages/UserProfile/EditProfileForm.js
+++ Client/src/components/Pages/UserProfile/EditProfileForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { updateProfile } from "../../../actions/users";

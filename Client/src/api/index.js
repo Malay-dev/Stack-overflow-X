529d8604758afd5bb9e9d91c3042b2dfd1b253e2
Malay <malaykumar2003@gmail.com>
2022-07-28 23:29:22 +0530

    url update

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" }); //https://stack-overflow-x.herokuapp.com
+const API = axios.create({ baseURL: "https://stack-overflow-x.herokuapp.com" }); //
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
